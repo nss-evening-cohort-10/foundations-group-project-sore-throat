b64da22bf16f993ce74d1eff1b7e50777e860c5f
Crystal Broach <crysland@gmail.com>
2019-09-17 18:49:04 -0500

    created div and array for cards

--- main.js
+++ main.js
@@ -13,7 +13,32 @@
 // Bio Page
 // *****************************************
 
-
+const bandMemberArr = [
+    {
+    memberImg: URL(),
+    memberName: 'Denise Deville',
+    memberRole: 'Violinist',
+    memberJoinDate: 'January 1999'
+    },
+    {
+    memberImg: URL(),
+    memberName: 'Jasmine Jamhardest',
+    memberRole: 'Drummer',
+    memberJoinDate: 'January 1999'
+    },
+    {
+    memberImg: URL(),
+    memberName: 'Alesha Allgood',
+    memberRole: 'Pianist/Vocalist',
+    memberJoinDate: 'January 1999'
+    },
+    {
+    memberImg: URL(),
+    memberName: 'Crystal Cantbehave',
+    memberRole: 'Guitarist/Vocalist',
+    memberJoinDate: 'January 1999'
+    },
+]
 
 
 
