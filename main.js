8f33200b65371a8d8f85adc1f65f3dfd46ea051b
Crystal Broach <crysland@gmail.com>
2019-09-21 09:48:07 -0500

    fixed classes to be more specific

--- main.js
+++ main.js
@@ -98,9 +98,9 @@ const bandMemberPrinter = (arr) => {
         domString +=
         `
         <div class="col-3">
-            <div class="card">
+            <div class="card bioCard">
             <img src="${thisObject.memberImg}" class="card-img-top img-fluid max-width: 100%" alt="...">
-            <div class="card-body">
+            <div class="card-body bioCard-body">
                 <h5 class="card-title">${thisObject.memberName}</h5>
                 <p class="card-text">${thisObject.memberRole}</p>
                 <p class="card-text">${thisObject.memberJoinDate}</p>
@@ -161,7 +161,7 @@ const albumPrinter = (arr) => {
             <img src="${thisObject.albumImage}" class="img-thumbnail">
             </div>
             <div class="col-8">
-            <div class="card-body">
+            <div class="card-body bioCard-body">
                 <h5 class="card-title">${thisObject.albumName}</h5>
                 <ol class="card-text">
                 `
